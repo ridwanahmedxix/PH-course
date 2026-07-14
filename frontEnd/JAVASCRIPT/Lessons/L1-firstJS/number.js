a1234604ce7b6827da24a6b3a0893068667df77b
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-15 05:33:18 +0600

    note add

--- frontEnd/JAVASCRIPT/Lessons/L1-firstJS/number.js
+++ frontEnd/JAVASCRIPT/Lessons/L1-firstJS/number.js
@@ -1,3 +1,10 @@
 // const numberString = "50";
 // const number = parseInt(numberString);
 // console.log(number);
+
+// ==============================================================
+
+// const numberValue = "836.74";
+// const number = parseInt(numberValue);
+// console.log(number);
+// console.log(typeof number);
