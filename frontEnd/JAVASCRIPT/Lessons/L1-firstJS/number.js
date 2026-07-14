8995c92718b8d43b97d12cae5e1e43f78e91ec5b
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-15 05:40:40 +0600

    Some Update

--- frontEnd/JAVASCRIPT/Lessons/L1-firstJS/number.js
+++ frontEnd/JAVASCRIPT/Lessons/L1-firstJS/number.js
@@ -8,3 +8,18 @@
 // const number = parseInt(numberValue);
 // console.log(number);
 // console.log(typeof number);
+
+// ==============================================================
+
+// const numberString = "40.8";
+// const number = parseFloat(numberString);
+// console.log(number);
+// console.log(typeof number);
+
+// ==============================================================
+
+const numberString = "40.4Bangladesh";
+// const numberString = "Bangladesh40.4";
+const number = parseFloat(numberString);
+console.log(number);
+console.log(typeof number);
