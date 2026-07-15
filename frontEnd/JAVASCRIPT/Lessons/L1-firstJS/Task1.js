5bb221516fcd219d37c3b65e64191269c34cf72b
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-15 06:50:50 +0600

    some update

---
+++ frontEnd/JAVASCRIPT/Lessons/L1-firstJS/Task1.js
@@ -0,0 +1,11 @@
+const fullName = " Ridwan Ahmed";
+const age = 19;
+let favoriteFood = " Mango";
+let favoriteLanguage = "TypeScript";
+var loveCoding = true;
+
+console.log("My name is :" + fullName);
+console.log("I am " + 19 + " years old");
+console.log("My favorite food is :" + favoriteFood);
+console.log("My favorite programming language is : " + favoriteLanguage);
+console.log("I love coding :" + loveCoding);
