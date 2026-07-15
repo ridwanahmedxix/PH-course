334a7ae7b593cfdbe629efecf1e723dcc7d9722b
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-15 06:34:36 +0600

    some update

---
+++ frontEnd/JAVASCRIPT/Lessons/L1-firstJS/adMath.js
@@ -0,0 +1,17 @@
+// let price = 600;
+// price = 400;
+
+// // price = price + 50;
+
+// // shortCut
+// price += 50;
+// console.log(price);
+
+let price = 600;
+price = 400;
+
+price = price - 50;
+
+// shortCut
+// price -= 50;
+console.log(price);
