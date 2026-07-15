f49c3efe163ada998ee589438ffcce82250ceb26
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-15 21:07:10 +0600

    Note add

---
+++ frontEnd/JAVASCRIPT/Lessons/L2-M8/if.js
@@ -0,0 +1,43 @@
+/**
+ * If  -
+ *
+ *
+ * Else
+ *
+ */
+
+// if (10 < 20) {
+//   console.log(" Hea Thik Ache Tumer Kotha");
+// }
+
+// if (5 <= 5) {
+//   console.log("Okay");
+// }
+
+// const eggPrice = 300;
+
+// if (eggPrice <= 500) {
+//   console.log(" You will buy this ");
+// }
+
+// =============================================
+
+// const age = 12;
+
+// if (age >= 18) {
+//   console.log(" This gaite for you AD");
+// } else {
+//   console.log("This gaite not for you ");
+// }
+
+// =============================================
+
+const age = 18;
+
+if (age >= 18) {
+  console.log(" Sobgula ride a apni chorte parbem");
+} else if (age >= 15) {
+  console.log(" Kiso ride a chorte parben ");
+} else {
+  console.log(" Kono ride a chorte parben na ");
+}
