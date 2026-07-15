a360039603ecd885450e4c81dc6829711955d2e0
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-15 06:04:04 +0600

    Some Update

--- frontEnd/JAVASCRIPT/Lessons/L1-firstJS/Math.js
+++ frontEnd/JAVASCRIPT/Lessons/L1-firstJS/Math.js
@@ -1,8 +1,26 @@
 // ===========================================================
-// MODULUS OPERATOR - %
+// 5. MODULUS OPERATION = %
 // ===========================================================
 
 // let num1 = 5;
 // let num2 = 2;
 // const result = num1 % num2;
 // console.log(result);
+
+// ===========================================================
+// 1. ADDITION OPERATION = +
+// ===========================================================
+
+// const laptopPrice = 5200;
+// const phonePrice = 2800;
+// const totalPrice = laptopPrice + phonePrice;
+// console.log(totalPrice);
+
+// ===========================================================
+// 2. SUBTRACTION OPERATION = -
+// ===========================================================
+
+// const myMoney = 150;
+// const eggPrice = 100;
+// const totalPrice = myMoney - eggPrice;
+// console.log(totalPrice);
