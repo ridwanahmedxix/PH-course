27b2843b04f8f6830cbbb7a96b7c19d7ec543125
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-16 14:39:21 +0600

    some update

---
+++ frontEnd/JAVASCRIPT/Task/Task-1-M8/task2.js
@@ -0,0 +1,19 @@
+// const isRaining = true;
+
+// if (isRaining) {
+//   console.log(" Take an umbrella");
+// } else {
+//   console.log("lagbe na ");
+// }
+
+// =========================================
+
+// const stock = 0;
+
+// if (stock === 0) {
+//   console.log("This product out of stock");
+// }
+
+// if (stock >= 0) {
+//   console.log("This product out of stock");
+// }
---
+++ frontEnd/JAVASCRIPT/Task/Task-1-M8/task8.js
@@ -0,0 +1,12 @@
+const age = 18;
+const userId = 203040;
+
+if (age >= 18) {
+  if (userId === 203040) {
+    console.log("You will be able to vote.");
+  } else {
+    console.log("Age is correct but username is wrong ");
+  }
+} else {
+  console.log("No, you won't be able to vote.");
+}
