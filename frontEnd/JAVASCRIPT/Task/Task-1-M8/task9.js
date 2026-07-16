9ea32e529776738c4f6ccee2b94ed6563b00e096
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-16 14:57:01 +0600

    some update

---
+++ frontEnd/JAVASCRIPT/Task/Task-1-M8/task9.js
@@ -0,0 +1,12 @@
+const isLoggedin = true;
+const isAdmin = true;
+
+if (isLoggedin === true) {
+  if (isAdmin === true) {
+    console.log("Admin Panel");
+  } else {
+    console.log("User Dashboard");
+  }
+} else {
+  console.log("Please Login");
+}
