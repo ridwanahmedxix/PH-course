7e0067c6dae3de03f80c1251a26836446c26c328
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-16 07:41:34 +0600

    Update

--- frontEnd/JAVASCRIPT/Lessons/L2-M8/else.js
+++ frontEnd/JAVASCRIPT/Lessons/L2-M8/else.js
@@ -1,7 +1,19 @@
-const riskaVara = 60;
+// const riskaVara = 60;
 
-if (riskaVara <= 50) {
-  console.log(" Mama ami Gulshan 2 a jabo");
+// if (riskaVara <= 50) {
+//   console.log(" Mama ami Gulshan 2 a jabo");
+// } else {
+//   console.log("Mama vara besi chaw keno ? ami local ");
+// }
+
+// ====================================================
+
+const x = 400;
+const y = 750;
+
+if (x !== y) {
+  const result = y + x;
+  console.log("The Value IS : ", result);
 } else {
-  console.log("Mama vara besi chaw keno ? ami local ");
+  console.log(" Whats Up");
 }
