6c4e8e4e0d6f24acc6720ad37534cf20f3c6f942
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-16 14:06:58 +0600

    Task3 complete

---
+++ frontEnd/JAVASCRIPT/Task/Task-1/task1.js
@@ -0,0 +1,18 @@
+const price1 = "500";
+const price2 = 500;
+
+// if (price1 == price2) {
+//   console.log("Compare ");
+// }
+
+// if (price1 === price2) {
+//   console.log("Same  ");
+// } else {
+//   console.log("Not same ");
+// }
+
+if (price1 == price2) {
+  console.log("Same  ");
+} else {
+  console.log("Not same ");
+}
---
+++ frontEnd/JAVASCRIPT/Task/Task-1/task3.js
@@ -0,0 +1,11 @@
+// POSITIVE OR NEGATIVE NUMBER CHECK
+
+const myNumber = 100;
+
+if (myNumber > 0) {
+  console.log("Positive Number");
+} else if (myNumber < 0) {
+  console.log(" Negative Number ");
+} else {
+  console.log("Zero");
+}
