6375048e7ba9b38710c3210bbea4140a11bd8b5a
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-16 08:20:38 +0600

    Nasted learn

---
+++ frontEnd/JAVASCRIPT/Lessons/L2-M8/Nested.js
@@ -0,0 +1,11 @@
+const money = 600;
+
+if (money >= 500) {
+  console.log("Kire mama , ajke khawya ");
+  console.log("Tk Nai ");
+  if (money > 800) {
+    console.log("check");
+  } else {
+    console.log(" Eto kiso jani , Khawya ");
+  }
+}
