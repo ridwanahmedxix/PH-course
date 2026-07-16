883213d17b2596f1d7423273e357c38dc3f0329f
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-16 07:53:40 +0600

    If Else

---
+++ frontEnd/JAVASCRIPT/Lessons/L2-M8/multiple.js
@@ -0,0 +1,11 @@
+// const salary = 50000;
+// const isWebDeveloper = true;
+// const isBoroLoks = false;
+
+// // if (salary <= 50000 && isWebDeveloper === true)
+
+// if ((salary <= 50000 && isWebDeveloper === true) || isBoroLoks === true) {
+//   console.log("Baba ami raji ");
+// } else {
+//   console.log("Dhorja dan dike ");
+// }
