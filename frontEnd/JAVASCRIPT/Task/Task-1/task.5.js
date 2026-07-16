7738f017717b6ec4c0e5ef770729d90c8ac3d99f
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-16 14:16:15 +0600

    Task 5 complete

---
+++ frontEnd/JAVASCRIPT/Task/Task-1/task.5.js
@@ -0,0 +1,8 @@
+const age = 18;
+const hasTicket = true;
+
+if (age >= 18 && hasTicket === true) {
+  console.log("Entry Allowed");
+} else {
+  console.log("Not Allowed");
+}
---
+++ frontEnd/JAVASCRIPT/Task/Task-1/task4.js
@@ -0,0 +1,9 @@
+const speed = 70;
+
+if (speed >= 80) {
+  console.log("Over Speed");
+} else if (speed >= 50) {
+  console.log("Normal Speed");
+} else {
+  console.log("Low Speed");
+}
