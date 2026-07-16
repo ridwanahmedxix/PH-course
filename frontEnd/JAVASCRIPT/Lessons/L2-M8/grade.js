17da6f546638422b594deadb459664efb78fe456
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-16 08:15:06 +0600

    Grade if else

---
+++ frontEnd/JAVASCRIPT/Lessons/L2-M8/grade.js
@@ -0,0 +1,10 @@
+const marks = 45;
+
+if (marks >= 80) {
+  console.log("Sabas , Tumi A+ Paiso ");
+}
+if (marks >= 60) {
+  console.log("Umm , chole mota moti ");
+} else {
+  console.log(" Tui fail Mokbul");
+}
