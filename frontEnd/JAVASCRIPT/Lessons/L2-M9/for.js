b379796f8ab2c1901b9830fb271189a7f14b2e16
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-17 20:54:39 +0600

    For and While Lopp basic learn

---
+++ frontEnd/JAVASCRIPT/Lessons/L2-M9/for.js
@@ -0,0 +1,16 @@
+// ===================================
+//  While Loop
+
+// let count = 0;
+
+// while (count <= 8) {
+//   console.log(`My Student Number Is ${count}`);
+//   count++;
+// }
+
+// ======================================
+// For Loop
+
+// for (let i = 0; i <= 20; i++) {
+//   console.log(` NUMBER - ${i}`);
+// }
---
+++ frontEnd/JAVASCRIPT/Lessons/L2-M9/miniTask.js
@@ -0,0 +1,24 @@
+// For Loop Task 1
+
+// for (let count = 0; count <= 10; count++) {
+//   console.log(`${count}`);
+// }
+
+// ==================================
+
+// While Loop Task 2
+
+// let count = 0;
+
+// while (count <= 5) {
+//   console.log(`${count}`);
+//   count++;
+// }
+
+// ==================================
+
+// For Loop Task 3
+
+for (let count = 10; count >= 1; count--) {
+  console.log(`${count}`);
+}
