9749e4583f08e151e13839e18ca6538902f4088c
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-18 07:42:30 +0600

    Break and Continue Learn

---
+++ frontEnd/JAVASCRIPT/Lessons/L2-M9/break.js
@@ -0,0 +1,9 @@
+// for (let count = 1; count <= 100; count++) {
+//   if (count > 20) {
+//     break;
+//   }
+
+//   console.log(count);
+// }
+
+// ! ========================================
---
+++ frontEnd/JAVASCRIPT/Lessons/L2-M9/continur.js
@@ -0,0 +1,18 @@
+// ! ============================
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 3) {
+//     continue;
+//   }
+//   console.log(" The Number Is I ", i);
+// }
+
+// ! ============================
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    continue;
+  }
+
+  console.log(" Number is ", i);
+}
