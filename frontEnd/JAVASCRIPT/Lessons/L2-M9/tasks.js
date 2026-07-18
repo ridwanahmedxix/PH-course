04e954251b16703aeea8aa83a2b5a74330d1c98e
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-18 17:34:24 +0600

    4 Task complete

--- frontEnd/JAVASCRIPT/Lessons/L2-M9/tasks.js
+++ frontEnd/JAVASCRIPT/Lessons/L2-M9/tasks.js
@@ -5,3 +5,25 @@
 // }
 
 // # =========== Task - 02 ===============
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log("Number :", i);
+// }
+
+// # =========== Task - 03 ===============
+
+// let count = 10;
+
+// while (count >= 1) {
+//   console.log(" Number :", count);
+//   count--;
+// }
+
+// # =========== Task - 04 ===============
+
+// let i = 1;
+
+// while (i <= 7) {
+//   console.log("Loop চলছে ", i);
+//   i++;
+// }
