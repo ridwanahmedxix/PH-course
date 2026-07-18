f57ddb1031af744ec48b55090af8db48a6e34bc0
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-18 16:50:41 +0600

    Do While Loop Learn

---
+++ frontEnd/JAVASCRIPT/Lessons/L2-M9/do-while.js
@@ -0,0 +1,20 @@
+// ! ==================================
+
+// ? While Loop
+
+// let i = 1;
+
+// while (i <= 10) {
+//   console.log("Hello World :", i);
+//   i++;
+// }
+
+// ! ==================================
+
+// ?  Do While Loop
+
+let i = 1;
+do {
+  console.log(" Hello World :", i);
+  i++;
+} while (i <= 10);
