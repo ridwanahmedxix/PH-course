2e9b6167660ee18284cf65b91ccc390edd8907cf
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-18 20:34:21 +0600

    Array start learning

---
+++ frontEnd/JAVASCRIPT/Lessons/L3-M10/array.js
@@ -0,0 +1,3 @@
+const team = [20, 30, 40, 50, 60];
+const playerName = ["sami", "mahi", "ali", "rafi", "ridwan"];
+console.log(playerName.length);
---
+++ frontEnd/JAVASCRIPT/Lessons/L3-M10/length.js
@@ -0,0 +1,10 @@
+// const fruits = ["mango", "banana", "pianaple", "apple", "jackFruit"];
+
+// console.log(fruits[1]);
+
+// # =================================
+
+const friends = ["ridwan", "dilip", "najmul", "shamim"];
+friends[2] = "sohel";
+// console.log(friends[3]);
+console.log(friends);
