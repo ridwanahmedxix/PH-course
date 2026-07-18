24e0b79be2db43725937587d2d77a267652175cf
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-18 06:23:22 +0600

    Revice

---
+++ frontEnd/JAVASCRIPT/Lessons/L2-M9/prac.js
@@ -0,0 +1,14 @@
+// For Loop
+
+// for (let count = 0; count <= 10; count++) {
+//   console.log(`${count}`);
+// }
+
+// While Loop
+
+let count = 0;
+
+while (count <= 10) {
+  console.log(`${count}`);
+  count++;
+}
