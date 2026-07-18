0fe0b51aea08530934be7acdc0a1377a335bb489
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-18 06:40:02 +0600

    Mini task complete

--- frontEnd/JAVASCRIPT/Lessons/L2-M9/prac.js
+++ frontEnd/JAVASCRIPT/Lessons/L2-M9/prac.js
@@ -3,12 +3,32 @@
 // for (let count = 0; count <= 10; count++) {
 //   console.log(`${count}`);
 // }
+// ! ====================================
+// While Loop
+
+// let count = 0;
+
+// while (count <= 10) {
+//   console.log(`${count}`);
+//   count++;
+// }
+
+// ! ====================================
+
+// Mini Task
+// For Loop
+
+//  for (let tree = 1; tree <= 9; tree++) {
+//   console.log(`Tree Number ${tree} A Water Daw `);
+// }
 
 // While Loop
 
-let count = 0;
+// let tree = 1;
 
-while (count <= 10) {
-  console.log(`${count}`);
-  count++;
-}
+// while (tree <= 9) {
+//   console.log(`Tree Number ${tree} A Water Daw `);
+//   tree++;
+// }
+
+// ! ====================================
