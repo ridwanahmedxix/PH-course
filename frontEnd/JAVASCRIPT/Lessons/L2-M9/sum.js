a24069a71d62b936fb8e0bfbfff46e8cc0585e47
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-18 07:16:30 +0600

    sum file create

---
+++ frontEnd/JAVASCRIPT/Lessons/L2-M9/sum.js
@@ -0,0 +1,12 @@
+// sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//   sum = sum + i;
+//   console.log(`${i} and ${sum} `);
+// }
+
+// ! ========================================
+
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
