de08d30314a5e1b88a71aaa0d244528021dd59e3
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-19 06:45:02 +0600

    Array Pop

---
+++ frontEnd/JAVASCRIPT/Lessons/L3-M10/pop.js
@@ -0,0 +1,28 @@
+// const fNumbers = [10, 23, 45, 67, 78];
+// console.log(fNumbers);
+// fNumbers.pop();
+// console.log(fNumbers);
+
+// # ===============================
+
+// const fNumbers = [10, 23, 45, 67, 78];
+// console.log(fNumbers);
+// fNumbers.pop();
+// console.log(fNumbers);
+// fNumbers.pop();
+// console.log(fNumbers);
+// fNumbers.pop();
+// console.log(fNumbers);
+// fNumbers.pop();
+// console.log(fNumbers);
+
+// # ===============================
+
+const fNumbers = [10, 23, 45, 67, 78];
+console.log(fNumbers);
+const firstOut = fNumbers.pop();
+console.log(fNumbers);
+const secondOut = fNumbers.pop();
+console.log(fNumbers);
+
+console.log(firstOut, secondOut);
