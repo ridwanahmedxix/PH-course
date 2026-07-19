fcc91613c0da95c4e77ba35adb97b1682357d7dd
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-19 06:37:49 +0600

    Array Push

--- frontEnd/JAVASCRIPT/Lessons/L3-M10/push.js
+++ frontEnd/JAVASCRIPT/Lessons/L3-M10/push.js
@@ -1,3 +1,6 @@
 const friends = ["Ridwan", "Talha", "Aideed", "Shamim"];
-
+friends.push("Dilip");
+// console.log(friends);
+friends[4] = "Dj Dilip";
+//  console.log(friends.length);
 console.log(friends);
