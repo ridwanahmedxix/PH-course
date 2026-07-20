4a3a4530e7aff651078bac690dcfe68ac18ae1b9
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-20 20:06:51 +0600

    some update

--- frontEnd/JAVASCRIPT/Lessons/L3-M10/includes.js
+++ frontEnd/JAVASCRIPT/Lessons/L3-M10/includes.js
@@ -1,5 +1,16 @@
 // #  =======================
 
-const myNumber = [12, 4, 66, 80, 2];
-const checkNum = myNumber.includes(0);
-console.log(checkNum);
+// const myNumber = [12, 4, 66, 80, 2];
+// const checkNum = myNumber.includes(0);
+// console.log(checkNum);
+
+// #  =======================
+
+// const players = ["Messi", "Emi", "Demaria", "Meradona "];
+// if (players.includes("Messi")) {
+//   console.log("Our messi is playing");
+// } else {
+//   console.log(" Your Favorite Player Not Playing ");
+// }
+
+// #  =======================
