da429d0785ed4569fe5df0f1985ab4411ca65cf8
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-20 20:32:06 +0600

    some update

---
+++ frontEnd/JAVASCRIPT/Lessons/L3-M10/for.js
@@ -0,0 +1,7 @@
+const numbers = [22, 34, 65, 89, 46, 35, 78];
+
+// const lenght = numbers.length;
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log("The number is :", i);
+}
--- frontEnd/JAVASCRIPT/Lessons/L3-M10/loop.js
+++ frontEnd/JAVASCRIPT/Lessons/L3-M10/loop.js
@@ -1,4 +1,12 @@
-const myNumbers = [22, 46, 78, 35, 67];
-for (const number of myNumbers) {
-  console.log(number);
+// const myNumbers = [22, 46, 78, 35, 67];
+// for (const number of myNumbers) {
+//   console.log(number);
+// }
+
+// # =======================================
+
+const players = ["Messi", "Emi", "Demaria", "Meradona "];
+
+for (const player of players) {
+  console.log(player);
 }
