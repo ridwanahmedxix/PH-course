f2176d80f0f4a61592d7494f2ff876e056c8b1f6
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-20 19:45:33 +0600

    first commit

---
+++ frontEnd/JAVASCRIPT/Lessons/L3-M10/shift-unS.js
@@ -0,0 +1,15 @@
+// #  ====== SHIFT ==========
+
+// const rNumbers = [1, 2, 3, 4, 5];
+// console.log(rNumbers);
+// rNumbers.shift();
+// console.log(rNumbers);
+
+// #  ====== UNSHIFT ==========
+
+const rNumbers = [1, 2, 3, 4, 5];
+console.log(rNumbers);
+rNumbers.shift();
+console.log(rNumbers);
+rNumbers.unshift(10);
+console.log(rNumbers);
