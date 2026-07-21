72df5bffabc266e3f5f7f64bfb08c03f704b5e9f
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-21 20:13:23 +0600

    some update

--- frontEnd/JAVASCRIPT/Lessons/L4-M11/comparison.js
+++ frontEnd/JAVASCRIPT/Lessons/L4-M11/comparison.js
@@ -1,8 +1,45 @@
-const subject = "Chemistry";
-const book = "chemistry";
+// const subject = "chemistry";
+// const book = "Chemistry";
 
-if (subject === book) {
-  console.log("Poira fatie felbo ");
-} else {
-  console.log("Auto pass chai ");
-}
+// // console.log(book.toLowerCase());
+
+// if (subject === book) {
+//   console.log("Poira fatie felbo ");
+// } else {
+//   console.log("Auto pass chai ");
+// }
+
+// # ================================
+
+// const subject = "chemistry";
+// const book = "Chemistry";
+
+// if (subject.toLowerCase() === book.toLowerCase()) {
+//   console.log("Poira fatie felbo ");
+// } else {
+//   console.log("Auto pass chai ");
+// }
+
+// # ================================
+
+// const userEmailProvide = "Ridwan@Gmail.com";
+// const userEmailSaveInDatabase = "ridwan@gmail.com";
+
+// if (userEmailProvide === userEmailSaveInDatabase) {
+//   console.log("Log in successful ");
+// } else {
+//   console.log("Please , try agian ");
+// }
+
+// * ------------------------------------------
+
+// const userEmailProvide = "Ridwan@Gmail.com";
+// const userEmailSaveInDatabase = "ridwan@gmail.com";
+
+// if (userEmailProvide.toLowerCase() === userEmailSaveInDatabase.toLowerCase()) {
+//   console.log("Log in successful ");
+// } else {
+//   console.log("Please , try agian ");
+// }
+
+// # ================================
