7744ee112b2915efa32383dbbc3e866f7343ea86
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-21 07:35:22 +0600

    Splice and slice learn

--- frontEnd/JAVASCRIPT/Lessons/L3-M10/slice.js
+++ frontEnd/JAVASCRIPT/Lessons/L3-M10/slice.js
@@ -3,5 +3,11 @@
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // const katlam = numbers.slice(1, 6);
 // console.log(katlam);
+// console.log(numbers);
 
-// # ======== SLICE =====================
+// # ======== SPLICE =====================
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const katlam = numbers.splice(1, 6);
+console.log(katlam);
+console.log(numbers);
