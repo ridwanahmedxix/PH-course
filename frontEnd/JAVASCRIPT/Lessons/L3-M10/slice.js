53a9b31f484ccd05518169d9b63493523f1faeae
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-21 07:32:49 +0600

    some update

---
+++ frontEnd/JAVASCRIPT/Lessons/L3-M10/slice.js
@@ -0,0 +1,7 @@
+// # ======== SLICE =====================
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const katlam = numbers.slice(1, 6);
+// console.log(katlam);
+
+// # ======== SLICE =====================
