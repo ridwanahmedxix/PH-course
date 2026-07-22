f0d42881d6dbd607a141a59ab79eb786967c454d
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-22 19:12:38 +0600

    Task 1 complete

---
+++ frontEnd/JAVASCRIPT/Lessons/L4-M11/task.js
@@ -0,0 +1,14 @@
+// * ================ Task - 01 ================
+
+// const string = "Hello";
+// console.log(string);
+
+const Arrays = ["H", "E", "L", "L", "O"];
+
+// Arrays[0] = "HEiii";
+// console.log(Arrays);
+
+Arrays.push("O");
+Arrays.push("!");
+
+console.log(Arrays);
