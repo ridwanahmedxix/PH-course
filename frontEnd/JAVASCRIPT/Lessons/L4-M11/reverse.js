f4617fc8f803c61ea061259eca2cc73e74c8cb8e
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-22 08:35:03 +0600

    Some update

--- frontEnd/JAVASCRIPT/Lessons/L4-M11/reverse.js
+++ frontEnd/JAVASCRIPT/Lessons/L4-M11/reverse.js
@@ -1,4 +1,4 @@
 const commitment = " I will work hard and never give up";
-
-const parts = commitment.split(" ");
-console.log(parts);
+const parts = commitment.split("");
+const reverseParts = parts.reverse();
+console.log(reverseParts);
