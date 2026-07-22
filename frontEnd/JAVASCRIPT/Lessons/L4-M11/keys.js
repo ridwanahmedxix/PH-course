390b2022af8a6b11242dcad780972c51a0c19d81
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-22 14:12:05 +0600

    Keys

---
+++ frontEnd/JAVASCRIPT/Lessons/L4-M11/keys.js
@@ -0,0 +1,10 @@
+const person = {
+  name: "Ridwan Ahmed",
+  age: 18,
+  isSingle: true,
+  pesha: "student",
+  " home address": "Rangpure",
+};
+
+const keys = Object.keys(person);
+console.log(keys);
