2ca040b2647736d71c35d462bb7d37b8d9cf8ccf
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-22 19:20:30 +0600

    Task 2 complete

--- frontEnd/JAVASCRIPT/Lessons/L4-M11/task.js
+++ frontEnd/JAVASCRIPT/Lessons/L4-M11/task.js
@@ -3,12 +3,26 @@
 // const string = "Hello";
 // console.log(string);
 
-const Arrays = ["H", "E", "L", "L", "O"];
+// const Arrays = ["H", "E", "L", "L", "O"];
 
 // Arrays[0] = "HEiii";
 // console.log(Arrays);
 
-Arrays.push("O");
-Arrays.push("!");
+// Arrays.push("O");
+// Arrays.push("!");
 
-console.log(Arrays);
+// console.log(Arrays);
+
+// * ================ Task - 01 ================
+
+// const languageName = "JavaScript";
+// const programmingName = languageName.split("");
+// console.log(programmingName);
+
+// const myCountry = "My Country Is Bangladesh ";
+// const country = myCountry.split("");
+// console.log(country);
+
+const myLove = ["R", "I", "D", "W", "A", "N"];
+const love = myLove.join("");
+console.log(love);
