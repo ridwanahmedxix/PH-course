15b99b1306ab38623304dd43cc1c078f1cb8e2a2
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-22 08:54:44 +0600

    Some update

--- frontEnd/JAVASCRIPT/Lessons/L4-M11/object.js
+++ frontEnd/JAVASCRIPT/Lessons/L4-M11/object.js
@@ -1,14 +1,15 @@
-const cap = [
-  {
-    name: "CR7 Cap",
-    price: 250,
-    color: "red",
-  },
-  {
-    name: "Laptop",
-    price: 5500,
-    color: "blue",
-  },
-];
+const cap = {
+  name: "CR7 Cap",
+  price: 250,
+  color: "red",
+};
 
 console.log(cap);
+
+const laptop = {
+  name: "Laptop",
+  price: 5500,
+  color: "blue",
+};
+
+console.log(laptop);
