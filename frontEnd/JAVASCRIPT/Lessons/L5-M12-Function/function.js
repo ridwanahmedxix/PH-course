a64d97c209097c1741152919ce09ca355b54c5a1
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-23 08:31:47 +0600

    Condition revition

---
+++ frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/function.js
@@ -0,0 +1,8 @@
+// const price = 25;
+// if (price >= 20) {
+//   console.log(" I will buy this ");
+// } else {
+//   console.log(" Not enough money ");
+// }
+
+// ! ===================================
