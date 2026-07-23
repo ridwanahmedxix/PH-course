b26cd45e5e53c6fad0335948c382411149178872
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-23 08:39:16 +0600

    Some update

---
+++ frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/function.js
@@ -0,0 +1,13 @@
+// const mySelf = () => {
+//   console.log(" My name is rockstar rakib ");
+// };
+
+// mySelf();
+
+// ! ======================================================
+
+function fanOn() {
+  return console.log(" Fan is on ");
+}
+
+console.log(fanOn());
