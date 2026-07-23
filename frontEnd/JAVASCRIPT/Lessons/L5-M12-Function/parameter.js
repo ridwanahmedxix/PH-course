4fb42320f0e66f2359fb205447b940eeba4ceb4c
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-23 20:08:30 +0600

    Square function create

--- frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/parameter.js
+++ frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/parameter.js
@@ -6,10 +6,27 @@
 
 // # ===================================================
 
-function add(num1, num2) {
-  console.log("You provided 2 number ", num1, num2);
-  const sum = num1 + num2;
-  console.log(" Your sum result :", sum);
-}
+// function add(num1, num2) {
+//   console.log("You provided 2 number ", num1, num2);
+//   const sum = num1 + num2;
+//   console.log(" Your sum result :", sum);
+// }
 
-add(11, 22);
+// add(11, 22);
+
+// # ===================================================
+
+// function examPle(num1, num2, num3) {
+//   const sum = num1 + num2 + num3;
+//   console.log("Your sum result :", sum);
+// }
+
+// examPle(1, 1, 4);
+
+// # ===================================================
+
+function square(number) {
+  const sqResult = number * number;
+  console.log(" Your square result :", sqResult);
+}
+square(10);
