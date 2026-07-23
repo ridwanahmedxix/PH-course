61d72e25bd335e44f8105732f40ed867d5b2c717
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-23 08:33:48 +0600

    Revision file create

--- frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/function.js
+++
@@ -1,8 +0,0 @@
-// const price = 25;
-// if (price >= 20) {
-//   console.log(" I will buy this ");
-// } else {
-//   console.log(" Not enough money ");
-// }
-
-// ! ===================================
---
+++ frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/revision.js
@@ -0,0 +1,12 @@
+// const price = 25;
+// if (price >= 20) {
+//   console.log(" I will buy this ");
+// } else {
+//   console.log(" Not enough money ");
+// }
+
+// ! ===================================
+
+for (let i = 1; i <= 10; i++) {
+  console.log("Number :", i);
+}
