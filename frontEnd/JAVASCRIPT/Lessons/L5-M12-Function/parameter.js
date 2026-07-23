062c8200f944d33f0f4ffcb4747d21e65c24e70b
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-23 19:59:32 +0600

    Function create

--- frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/parameter.js
+++ frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/parameter.js
@@ -5,3 +5,9 @@
 // sayHello("Ridwan");
 
 // # ===================================================
+
+function add(num1, num2) {
+  console.log("You provided 2 number ", num1, num2);
+}
+
+add(11, 22);
