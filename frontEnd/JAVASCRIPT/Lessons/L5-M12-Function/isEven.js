b9f3fa42cdc67b7957f445869f31f9ac34e04983
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-24 20:03:36 +0600

    Even and Odd number function create

--- frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/isEven.js
+++ frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/isEven.js
@@ -1,5 +1,12 @@
 function isEven(number) {
   const result = number % 2;
+
+  if (number % 2 === 0) {
+    console.log(" This is a Even Number ", result);
+  } else {
+    console.log(" This is a Odd Number ");
+  }
   return result;
 }
-console.log(isEven(99));
+
+const sentNumber = isEven(100);
