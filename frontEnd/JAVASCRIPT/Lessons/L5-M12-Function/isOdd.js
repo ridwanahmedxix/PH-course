f3ccd49c2645820b1e869bd4ce14939bc4277762
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-24 20:34:04 +0600

    Odd number found function create

---
+++ frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/isOdd.js
@@ -0,0 +1,9 @@
+function isOdd(number) {
+  if (number % 2 === 1) {
+    console.log("This is a Odd number :", number);
+  } else {
+    console.log("This is a Even number :", number);
+  }
+}
+
+const result = isOdd(20);
