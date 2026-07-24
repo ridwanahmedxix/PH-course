0c9ea908a0772ac95face4a1f689d4523d850393
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-24 19:55:58 +0600

    recape file create

---
+++ frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/recape.js
@@ -0,0 +1,9 @@
+function doMath(a, b, c, d) {
+  const firstMAth = a + b;
+  const secondMath = c + d;
+  const multiply = firstMAth * secondMath;
+  return multiply;
+}
+
+const result = doMath(2, 2, 2, 2);
+console.log(result);
