a4aa9e23b96513a5e5e9e222b2d27a8fafe0aedf
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-25 09:10:17 +0600

    some update

--- frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/arrySum.js
+++ frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/arrySum.js
@@ -1,5 +1,6 @@
 const numbers = [22, 34, 56, 78, 98, 64, 34];
-
-for (const number of numbers) {
-  console.log(number);
+let sum = 0;
+for (const num of numbers) {
+  sum = sum + num;
+  console.log(num, sum);
 }
