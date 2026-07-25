d3dcf4c8d9a0906c532c3f5982e3d9e36d79be41
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-25 09:23:00 +0600

    Some Update

--- frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/evens.js
+++ frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/evens.js
@@ -2,8 +2,10 @@ const numbers = [12, 14, 15, 26, 25, 45, 31];
 
 for (num of numbers) {
   //   console.log(num);
+  const evens = [];
   if (num % 2 === 0) {
     console.log("This a even number", num);
+    evens.push(num);
   } else {
     console.log("Odd number", num);
   }
