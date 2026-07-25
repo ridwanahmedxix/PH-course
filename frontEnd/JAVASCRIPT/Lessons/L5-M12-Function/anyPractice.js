0a81995af3d34e66c0e688ad0952ff4a48e19d4a
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-25 09:17:34 +0600

    Some Update

--- frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/anyPractice.js
+++ frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/anyPractice.js
@@ -1,1 +1,9 @@
 const nums = [10, 11, 12, 13, 14, 15, 16];
+
+for (const number of nums) {
+  if (number % 2 === 0) {
+    console.log("This is a Even Number :", number);
+  } else {
+    console.log("This is a Odd Number :", number);
+  }
+}
