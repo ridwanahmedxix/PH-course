a3a09e00685b25f98cdcdee2844be452fa7ed346
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-25 18:58:30 +0600

    Vot function create

--- frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/task.js
+++ frontEnd/JAVASCRIPT/Lessons/L5-M12-Function/task.js
@@ -1,8 +1,19 @@
 // * ============== TASK - 01 ==============
 
-function Area(width, height) {
-  const area = width * height;
-  return area;
+// function Area(width, height) {
+//   const area = width * height;
+//   return area;
+// }
+// console.log(Area(20, 40));
+
+// * ============== TASK - 02 ==============
+
+function Vot(age) {
+  if (age >= 18) {
+    console.log("You can do vot");
+  } else {
+    console.log("You can not do vot");
+  }
 }
 
-console.log(Area(20, 40));
+Vot(19);
