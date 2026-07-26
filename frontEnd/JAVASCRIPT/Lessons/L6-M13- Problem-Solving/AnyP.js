37943098111504a1e2084aec3206222671a8d730
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-26 08:55:45 +0600

    Again solve problem

--- frontEnd/JAVASCRIPT/Lessons/L6-M13- Problem-Solving/AnyP.js
+++ frontEnd/JAVASCRIPT/Lessons/L6-M13- Problem-Solving/AnyP.js
@@ -6,14 +6,36 @@
 
 // # =========================================================
 
-const numbers = [12];
+// const numbers = [12];
 
-for (num of numbers) {
-  const events = [];
-  if (num % 2 === 0) {
-    console.log("This is a Even Number  :", num);
-    events.push();
+// for (num of numbers) {
+//   const events = [];
+//   if (num % 2 === 0) {
+//     console.log("This is a Even Number  :", num);
+//     events.push();
+//   } else {
+//     console.log("This is a Odd Number  :", num);
+//   }
+// }
+
+// # ========================================================
+
+// let numBer = 11;
+
+// if (numBer % 2 === 0) {
+//   console.log("Even :", numBer);
+// } else {
+//   console.log("Odd :", numBer);
+// }
+
+// # ========================================================
+
+function evenOdd(numBer) {
+  if (numBer % 2 === 0) {
+    console.log("Even :", numBer);
   } else {
-    console.log("This is a Odd Number  :", num);
+    console.log("Odd :", numBer);
   }
 }
+
+evenOdd(11);
--- frontEnd/JAVASCRIPT/Lessons/L6-M13- Problem-Solving/problem-01.js
+++ frontEnd/JAVASCRIPT/Lessons/L6-M13- Problem-Solving/problem-01.js
@@ -1,14 +1,27 @@
 //  EvenOdd Checker & Sum of a Range
 
-const numbers = [11, 12, 13, 14, 15, 16];
+// const numbers = [11, 12, 13, 14, 15, 16];
 
-for (num of numbers) {
-  const evens = [];
+// for (num of numbers) {
+//   const evens = [];
 
-  if (num % 2 === 0) {
-    console.log("This is a Even Number", num);
-    evens.push();
+//   if (num % 2 === 0) {
+//     console.log("This is a Even Number", num);
+//     evens.push();
+//   } else {
+//     console.log("This is a Odd Number", num);
+//   }
+// }
+
+// # =================================================================
+
+function evenOdd(numBer) {
+  if (numBer % 2 === 0) {
+    return "Even Number";
   } else {
-    console.log("This is a Odd Number", num);
+    return "Odd Number ";
   }
 }
+
+console.log(evenOdd(11));
+console.log(evenOdd(22));
