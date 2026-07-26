278ec210ce38383415eb04abed0eff5544ed42d3
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-26 08:36:38 +0600

    Problem 01 solve

---
+++ frontEnd/JAVASCRIPT/Lessons/L6-M13- Problem-Solving/problem-01.js
@@ -0,0 +1,14 @@
+//  EvenOdd Checker & Sum of a Range
+
+const numbers = [11, 12, 13, 14, 15, 16];
+
+for (num of numbers) {
+  const evens = [];
+
+  if (num % 2 === 0) {
+    console.log("This is a Even Number", num);
+    evens.push();
+  } else {
+    console.log("This is a Odd Number", num);
+  }
+}
