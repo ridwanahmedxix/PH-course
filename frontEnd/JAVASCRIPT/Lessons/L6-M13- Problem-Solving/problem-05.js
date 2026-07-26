700a96f6ac200729049e0063e5779e715f89dad9
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-26 14:06:24 +0600

    Problem 05 solve

---
+++ frontEnd/JAVASCRIPT/Lessons/L6-M13- Problem-Solving/problem-05.js
@@ -0,0 +1,8 @@
+// ? Reverse a String
+
+const commitment = " I Love Bangladesh ";
+const parts = commitment.split("");
+const reverseParts = parts.reverse();
+const reverseCommitment = reverseParts.join("");
+
+console.log(reverseCommitment);
