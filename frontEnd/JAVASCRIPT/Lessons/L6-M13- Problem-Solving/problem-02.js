b0554e07d29f296c8d57ed479e2a27d3f2f0d1dd
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-26 09:13:44 +0600

    Problem 02 Solve

--- frontEnd/JAVASCRIPT/Lessons/L6-M13- Problem-Solving/problem-02.js
+++ frontEnd/JAVASCRIPT/Lessons/L6-M13- Problem-Solving/problem-02.js
@@ -13,3 +13,15 @@
 // console.log(sum);
 
 // ! ===================================================
+
+function sumRange(number) {
+  let sum = 0;
+  for (let i = 1; i <= number; i++) {
+    sum = sum + i;
+    console.log(i);
+  }
+
+  return sum;
+}
+
+console.log(sumRange(7));
