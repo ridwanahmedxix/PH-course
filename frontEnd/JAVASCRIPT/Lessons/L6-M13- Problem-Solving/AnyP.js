64698609290a4c37ef0cb0433fbe7abdc6e0fd0e
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-26 08:47:19 +0600

    Again solve problem

--- frontEnd/JAVASCRIPT/Lessons/L6-M13- Problem-Solving/AnyP.js
+++ frontEnd/JAVASCRIPT/Lessons/L6-M13- Problem-Solving/AnyP.js
@@ -5,3 +5,15 @@
 // console.log(sumMath(10, 20));
 
 // # =========================================================
+
+const numbers = [12];
+
+for (num of numbers) {
+  const events = [];
+  if (num % 2 === 0) {
+    console.log("This is a Even Number  :", num);
+    events.push();
+  } else {
+    console.log("This is a Odd Number  :", num);
+  }
+}
