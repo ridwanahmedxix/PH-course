22c2ffa5909cf5146451b232e26c8141ff1b71ee
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-28 20:00:27 +0600

    Some Update

--- frontEnd/JAVASCRIPT/Practice/p1.js
+++ frontEnd/JAVASCRIPT/Practice/p1.js
@@ -4,9 +4,14 @@
 
 // What is the output of the following code?
 
-let age = 18;
-if (age > 18) {
-  console.log("Adult");
-} else {
-  console.log("Minor");
+// let age = 18;
+// if (age > 18) {
+//   console.log("Adult");
+// } else {
+//   console.log("Minor");
+// }
+
+function add(a, b) {
+  return a + b;
 }
+console.log(add(5));
