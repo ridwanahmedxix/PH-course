fc02fdc1e27062f0aa696f670db5c5d1d1ac4bf7
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-28 20:24:06 +0600

    problem 4 solve

---
+++ frontEnd/JAVASCRIPT/Lessons/L7-M14-Bug/allProblems.js
@@ -0,0 +1,10 @@
+// Task 14-4: Debug a Broken Array Problem
+// ? This function should return the first element of an array. It currently returns the wrong item.
+
+// Bug: find why this doesn't return the first element
+
+// function getFirstElement(arr) {
+//   return arr[0]; // find the bug
+// }
+
+// console.log(getFirstElement([10, 20, 30])); // Expected: 10 (currently returns 20)
