de72815821b653f46b5ad4f0c009703f646cf658
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-28 20:13:57 +0600

    Problem 01 all Snippet solve

--- frontEnd/JAVASCRIPT/Lessons/L7-M14-Bug/p1.js
+++ frontEnd/JAVASCRIPT/Lessons/L7-M14-Bug/p1.js
@@ -1,7 +1,24 @@
 // ? Spot the Error Type
 
-function sayHi(name) {
-  console.log("Hi, " + name); // find the bug
-}
+// * Snippet A
 
-sayHi("Mira");
+// function sayHi(name) {
+//   console.log("Hi, " + name); // find the bug
+// }
+
+// sayHi("Mira");
+
+// * Snippet B
+
+// let word = "hello";
+// console.log(word.toUpperCase()); // find the bug
+
+// * Snippet C
+
+// ! + - i change the *
+
+// function square(num) {
+//   return num * num; // find the bug
+// }
+
+// console.log(square(4)); // should be 16
