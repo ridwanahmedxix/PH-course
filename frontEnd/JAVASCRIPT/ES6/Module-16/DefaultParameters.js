1866875bdee98f7c613631e267f547e2b4651cce
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-01 19:35:20 +0600

    Coffee Function Create

--- frontEnd/JAVASCRIPT/ES6/Module-16/DefaultParameters.js
+++ frontEnd/JAVASCRIPT/ES6/Module-16/DefaultParameters.js
@@ -1,5 +1,13 @@
-function greet(name = "Guest ") {
-  return `Hello ${name}`;
+// function greet(name = "Guest ") {
+//   return `Hello ${name}`;
+// }
+// console.log(greet("Ridwan"));
+// console.log(greet());
+
+// ! ========================================
+
+function makeCoffe(suger = 1) {
+  return `Your coffe with ${suger} tabel chamosh `;
 }
-console.log("Ridwan");
-console.log(greet());
+console.log(makeCoffe(3));
+console.log(makeCoffe());
