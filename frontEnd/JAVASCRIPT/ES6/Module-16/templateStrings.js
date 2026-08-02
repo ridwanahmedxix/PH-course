13f014e2af830b3cbeac8af5ebc286c594083dd0
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-02 07:14:59 +0600

    Some update

--- frontEnd/JAVASCRIPT/ES6/Module-16/templateStrings.js
+++ frontEnd/JAVASCRIPT/ES6/Module-16/templateStrings.js
@@ -8,8 +8,9 @@
 // ! =========================================================
 
 function addmitionConfirmd(name, amount) {
-  let message = ` What's up ${name} ? Your payment amount is : ${amount} `;
+  let message = ` What's up ${name || "Student"} ? Your payment amount is : ${amount} `;
   return message;
 }
 
 console.log(addmitionConfirmd("Ridwan", 3600));
+console.log(addmitionConfirmd(undefined, 3600));
