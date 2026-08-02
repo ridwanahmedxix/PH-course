9840a90b694400bc4217d833cefb3cc0249de4b9
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-02 07:14:08 +0600

    Some update

--- frontEnd/JAVASCRIPT/ES6/Module-16/templateStrings.js
+++ frontEnd/JAVASCRIPT/ES6/Module-16/templateStrings.js
@@ -1,3 +1,12 @@
+// function addmitionConfirmd(name, amount) {
+//   let message = ` What's up ${name} ? Your payment amount is : ${amount} `;
+//   return message;
+// }
+
+// console.log(addmitionConfirmd("Ridwan", 3600));
+
+// ! =========================================================
+
 function addmitionConfirmd(name, amount) {
   let message = ` What's up ${name} ? Your payment amount is : ${amount} `;
   return message;
