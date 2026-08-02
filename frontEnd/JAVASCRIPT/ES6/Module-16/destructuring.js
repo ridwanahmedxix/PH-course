ec3a7cfe7399cb0b4a7c8f96185874a125ff7331
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-02 19:13:44 +0600

    Some update

--- frontEnd/JAVASCRIPT/ES6/Module-16/destructuring.js
+++ frontEnd/JAVASCRIPT/ES6/Module-16/destructuring.js
@@ -1,2 +1,12 @@
-const numBers = [10, 23, 45, 67, 78, 65];
-console.log(numBers[2]);
+// const numBers = [10, 23, 45, 67, 78, 65];
+// const [ten, t3, , , , t6] = numBers;
+// console.log(ten, t3, t6);
+
+const student = {
+  name: "Ridwan Ahmed",
+  age: 18,
+  favoriteGame: "Minecraft",
+};
+
+const { name, age, favoriteGame } = student;
+console.log(name, age, favoriteGame);
