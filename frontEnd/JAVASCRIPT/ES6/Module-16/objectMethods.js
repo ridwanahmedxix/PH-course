6b7e36c50e3e7541af7b53c47b15c1432316402b
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-02 19:21:19 +0600

    Some update

---
+++ frontEnd/JAVASCRIPT/ES6/Module-16/objectMethods.js
@@ -0,0 +1,11 @@
+const student = {
+  name: "Ridwan",
+  age: 18,
+  id: 1122,
+};
+
+const keys = Object.keys(student);
+const values = Object.values(student);
+
+console.log(keys);
+console.log(values);
