1413f64db6f8c841afdf26ae4e10f32960f224fb
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-02 07:22:19 +0600

    max and min number

--- frontEnd/JAVASCRIPT/ES6/Module-16/spreadAndRestOperator.js
+++ frontEnd/JAVASCRIPT/ES6/Module-16/spreadAndRestOperator.js
@@ -1,3 +1,8 @@
-let numBer = [12, 23, 45, 67, 87, 54];
-console.log(numBer);
-console.log(...numBer);
+let numBer = [12, 23, 45, 67, 87, 54, 100];
+// console.log(numBer);
+// console.log(...numBer);
+
+let maxNumber = Math.max(...numBer);
+let minNumber = Math.min(...numBer);
+console.log(maxNumber);
+console.log(minNumber);
