535e6ad28b1e60da8c1a0a780ebb6aa589d7b0f6
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-02 07:12:51 +0600

    addmitionConfirmd Function Create

--- frontEnd/JAVASCRIPT/ES6/Module-16/templateStrings.js
+++ frontEnd/JAVASCRIPT/ES6/Module-16/templateStrings.js
@@ -1,4 +1,6 @@
 function addmitionConfirmd(name, amount) {
-  let message = ` What's up ${name} ? Your payment amount is : {amount} `;
+  let message = ` What's up ${name} ? Your payment amount is : ${amount} `;
   return message;
 }
+
+console.log(addmitionConfirmd("Ridwan", 3600));
