0edcf36a1dbf17a4b0abe04aa0d498beb565fdcb
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-03 19:13:41 +0600

    Some update

--- frontEnd/JAVASCRIPT/ES6/Module-17/dataTypes.js
+++ frontEnd/JAVASCRIPT/ES6/Module-17/dataTypes.js
@@ -1,3 +1,13 @@
 let name = "Ridwan Ahmed";
 let age = 18;
 let isMarried = false;
+let sallry = null;
+let gf = undefined;
+
+console.log(
+  typeof name,
+  typeof age,
+  typeof isMarried,
+  typeof sallry,
+  typeof gf,
+);
