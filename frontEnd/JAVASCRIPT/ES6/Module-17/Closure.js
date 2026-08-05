b9b037d0395f59f671ef9b267c4b2afdce25de45
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-05 06:54:00 +0600

    Some update

--- frontEnd/JAVASCRIPT/ES6/Module-17/Closure.js
+++ frontEnd/JAVASCRIPT/ES6/Module-17/Closure.js
@@ -9,3 +9,5 @@ function cashRegister() {
 
 let coffeshopCashCounter = cashRegister();
 console.log(coffeshopCashCounter(250));
+console.log(coffeshopCashCounter(100));
+console.log(coffeshopCashCounter(700));
