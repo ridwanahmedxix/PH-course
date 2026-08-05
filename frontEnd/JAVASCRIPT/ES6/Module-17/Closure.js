6682349c9bd3a00202990bb24c2fd779f1a222e4
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-05 06:46:21 +0600

    function create

--- frontEnd/JAVASCRIPT/ES6/Module-17/Closure.js
+++ frontEnd/JAVASCRIPT/ES6/Module-17/Closure.js
@@ -2,7 +2,10 @@ function cashRegister() {
   let amount = 0;
 
   return function (recevieAmount) {
-    amount + recevieAmount;
+    amount += recevieAmount;
     return amount;
   };
 }
+
+let coffeshopCashCounter = cashRegister();
+console.log(coffeshopCashCounter(250));
