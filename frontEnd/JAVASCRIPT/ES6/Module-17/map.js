32216646b3e1c04f45c149b3311060eaae1efcdb
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-06 08:37:53 +0600

    map loop create

--- frontEnd/JAVASCRIPT/ES6/Module-17/map.js
+++ frontEnd/JAVASCRIPT/ES6/Module-17/map.js
@@ -1,6 +1,16 @@
+// let numBers = [2, 4, 6, 8, 10];
+// let double = [];
+// for (let i = 0; i < numBers.length; i++) {
+//   double.push(numBers[i] * 2);
+// }
+// console.log(double);
+
+// ! ================================================
+
 let numBers = [2, 4, 6, 8, 10];
-let double = [];
-for (let i = 0; i < numBers.length; i++) {
-  double.push(numBers[i] * 2);
-}
+
+const double = numBers.map(function (element) {
+  return element * 2;
+});
+
 console.log(double);
