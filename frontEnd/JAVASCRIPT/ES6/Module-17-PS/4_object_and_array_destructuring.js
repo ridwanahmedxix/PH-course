181055c3a4ad8441859cc3e8e5ddfc99856b4555
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-07 07:34:26 +0600

    4_object_and_array_destructuring

--- frontEnd/JAVASCRIPT/ES6/Module-17-PS/4_object_and_array_destructuring.js
+++ frontEnd/JAVASCRIPT/ES6/Module-17-PS/4_object_and_array_destructuring.js
@@ -8,7 +8,39 @@
 */
 
 const responseExtractor = (obj) => {
-  console.log(obj);
+  const {
+    user: { name: userName, age = 18 },
+  } = obj;
+
+  console.log(userName, age);
 };
 
 console.log(responseExtractor({ user: { name: "Rafi", age: 22 } }));
+console.log(responseExtractor({ user: { name: "Rafi" } }));
+// console.log(responseExtractor({ user: { name: "" } }));
+
+// # ======================================================
+
+/*
+  Problem 2: Swap & Rest Extractor
+  Swap two variables and extract first/rest elements from an array
+  using destructuring, no temp variable.
+
+  Example: [a, b] = [b, a]
+  Example: [first, ...rest] = [10,20,30,40] -> first=10, rest=[20,30,40]
+*/
+
+let a = 20;
+let b = 30;
+
+// [b, a] = [a, b];
+[a, b] = [b, a];
+
+console.log(a, b);
+
+// -------------------------------------------
+
+let nums = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+let [first, ...rest] = nums;
+console.log(first, rest);
