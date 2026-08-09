e8d0e60a24c045587a706b397135b540d2c5a9ef
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-09 21:36:30 +0600

    Problem 1 file create

---
+++ frontEnd/JAVASCRIPT/Assignments/Assignment-03/pb-01.js
@@ -0,0 +1,13 @@
+function studentIntroduction(student) {
+  const studentData = {
+    name: "Rafi",
+    age: 18,
+    course: "JavaScript",
+  };
+
+  console.log(
+    `My name is ${name}. I am ${age} years old. I am learning ${course}.`,
+  );
+}
+
+studentIntroduction();
