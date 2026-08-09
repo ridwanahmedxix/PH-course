2115147d3b09ef2a5ea0759b72073be55da69dc6
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-09 20:55:02 +0600

    Some Update

--- frontEnd/JAVASCRIPT/ES6/Module-19-PS/19.1-2-Part-2.js
+++ frontEnd/JAVASCRIPT/ES6/Module-19-PS/19.1-2-Part-2.js
@@ -1,39 +1,39 @@
-// /*
-//   Problem 2: Passing Students Filter
-//   getPassingStudents(students, threshold) — return only the students
-//   whose marks are greater than or equal to the given threshold.
+/*
+  Problem 2: Passing Students Filter
+  getPassingStudents(students, threshold) — return only the students
+  whose marks are greater than or equal to the given threshold.
 
-//   Input:
-//     students = [
-//       { name: "Rafi", marks: 75 },
-//       { name: "Karim", marks: 40 },
-//       { name: "Utsho", marks: 60 },
-//     ]
-//     threshold = 60
+  Input:
+    students = [
+      { name: "Rafi", marks: 75 },
+      { name: "Karim", marks: 40 },
+      { name: "Utsho", marks: 60 },
+    ]
+    threshold = 60
 
-//   Output:
-//     [
-//       { name: "Rafi", marks: 75 },
-//       { name: "Utsho", marks: 60 },
-//     ]
-// */
+  Output:
+    [
+      { name: "Rafi", marks: 75 },
+      { name: "Utsho", marks: 60 },
+    ]
+*/
 
-// const getPassingStudents = (students, threshold) => {
-//   let filterStudent = students.filter((student) => {
-//     console.log(student);
-//     if (student.marks >= threshold) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   });
-//   console.log(filterStudent);
-// };
+const getPassingStudents = (students, threshold) => {
+  let filterStudent = students.filter((student) => {
+    console.log(student);
+    if (student.marks >= threshold) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  console.log(filterStudent);
+};
 
-// let studentsData = [
-//   { name: "Rafi", marks: 75 },
-//   { name: "Karim", marks: 40 },
-//   { name: "Utsho", marks: 60 },
-// ];
+let studentsData = [
+  { name: "Rafi", marks: 75 },
+  { name: "Karim", marks: 40 },
+  { name: "Utsho", marks: 60 },
+];
 
-// console.log(getPassingStudents(studentsData, 50));
+console.log(getPassingStudents(studentsData, 50));
