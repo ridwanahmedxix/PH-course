0ecaf4455116e92713f6ed795d4a260976bb81d2
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-10 20:32:59 +0600

    Problem 03 solve

--- frontEnd/JAVASCRIPT/Assignments/Assignment-03/final.js
+++ frontEnd/JAVASCRIPT/Assignments/Assignment-03/final.js
@@ -24,3 +24,31 @@ function filterActiveUsers(users) {
   }
   return users.filter((user) => user.isActive === true);
 }
+
+function countHashtags(caption) {
+  if (typeof caption !== "string") {
+    return "Invalid";
+  }
+
+  const words = caption.split(" ");
+
+  let hashTagCount = 0;
+  let longestHashTag = "";
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].startsWith("#")) {
+      hashTagCount++;
+
+      const tag = words[i].slice(1);
+
+      if (tag.length > longestHashTag.length) {
+        longestHashTag = tag;
+      }
+    }
+  }
+
+  return {
+    hashtagCount: hashTagCount,
+    longestTag: longestHashTag,
+  };
+}
--- frontEnd/JAVASCRIPT/Assignments/Assignment-03/pb-03.js
+++ frontEnd/JAVASCRIPT/Assignments/Assignment-03/pb-03.js
@@ -2,4 +2,25 @@ function countHashtags(caption) {
   if (typeof caption !== "string") {
     return "Invalid";
   }
+
+  const words = caption.split(" ");
+
+  let hashTagCount = 0;
+  let longestHashTag = "";
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].startsWith("#")) {
+      hashTagCount++;
+      const tag = words[i].slice(1);
+
+      if (tag.length > longestHashTag.length) {
+        longestHashTag = tag;
+      }
+    }
+  }
+
+  return {
+    hashTagCount: hashTagCount,
+    longestHashTag: longestHashTag,
+  };
 }
+console.log(countHashtags("Loving this weather today #sunny #vibes #weekend"));
