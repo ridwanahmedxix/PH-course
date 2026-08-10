caf0db3d64b478261db2a1ae41d2878639a7e045
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-10 20:40:43 +0600

    Some code update

--- frontEnd/JAVASCRIPT/Assignments/Assignment-03/final.js
+++ frontEnd/JAVASCRIPT/Assignments/Assignment-03/final.js
@@ -52,3 +52,55 @@ function countHashtags(caption) {
     longestTag: longestHashTag,
   };
 }
+
+function bonusScore(scores) {
+  if (!Array.isArray(scores) || scores.length === 0) {
+    return "Invalid";
+  }
+
+  for (let i = 0; i < scores.length; i++) {
+    if (typeof scores[i] !== "number") {
+      return "Invalid";
+    }
+  }
+
+  const updatedScores = scores.map(function (score) {
+    return score + 10;
+  });
+
+  const totalScore = updatedScores.reduce(function (total, score) {
+    return total + score;
+  }, 0);
+
+  return totalScore;
+}
+
+function generateLeaderboard(students) {
+  if (!Array.isArray(students) || students.length === 0) {
+    return "Invalid";
+  }
+
+  for (let i = 0; i < students.length; i++) {
+    const student = students[i];
+
+    if (
+      typeof student !== "object" ||
+      student === null ||
+      !student.hasOwnProperty("name") ||
+      !student.hasOwnProperty("score") ||
+      typeof student.score !== "number"
+    ) {
+      return "Invalid";
+    }
+  }
+
+  const qualified = students.filter((student) => {
+    return student.score >= 70;
+  });
+
+  const names = qualified.map((student) => {
+    return student.name.toUpperCase();
+  });
+
+  return names.slice(0, 3);
+}
