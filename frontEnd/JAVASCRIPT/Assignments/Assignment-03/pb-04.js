0961df0bb579ba188fb6024ac9e293c87130d32e
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-11 21:30:34 +0600

    Function condition add

--- frontEnd/JAVASCRIPT/Assignments/Assignment-03/pb-04.js
+++ frontEnd/JAVASCRIPT/Assignments/Assignment-03/pb-04.js
@@ -1,1 +1,8 @@
-function bonusScore(scores) {}
+function bonusScore(scores) {
+  if (!Array.isArray(scores) || scores.length === 0) {
+    return "Invalid";
+  }
+  if (scores.length === 0) {
+    return "Invalid";
+  }
+}
