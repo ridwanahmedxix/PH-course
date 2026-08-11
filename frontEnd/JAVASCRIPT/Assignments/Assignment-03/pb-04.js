c8ed884ab2c48a8711d99c113627e482bf822241
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-11 21:32:50 +0600

    Function 3rd  condition add

--- frontEnd/JAVASCRIPT/Assignments/Assignment-03/pb-04.js
+++ frontEnd/JAVASCRIPT/Assignments/Assignment-03/pb-04.js
@@ -2,7 +2,7 @@ function bonusScore(scores) {
   if (!Array.isArray(scores) || scores.length === 0) {
     return "Invalid";
   }
-  if (scores.length === 0) {
+  if (!scores.every((score) => typeof score === "number")) {
     return "Invalid";
   }
 }
