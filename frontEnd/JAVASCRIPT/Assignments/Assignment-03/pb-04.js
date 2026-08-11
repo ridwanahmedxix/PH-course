fecc64bc805a87eb7a73a88bd46f161946f6141f
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-11 21:38:29 +0600

    Problem 04 solve

--- frontEnd/JAVASCRIPT/Assignments/Assignment-03/pb-04.js
+++ frontEnd/JAVASCRIPT/Assignments/Assignment-03/pb-04.js
@@ -8,4 +8,8 @@ function bonusScore(scores) {
 
   const updateScore = scores.map((score) => score + 10);
   const total = updateScore.reduce((sum, score) => sum + score, 0);
+
+  return total;
 }
+
+console.log(bonusScore([80, 65, 90, 75]));
