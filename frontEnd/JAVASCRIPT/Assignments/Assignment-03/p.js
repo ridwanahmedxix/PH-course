3cc8d0d2c6d9180efeebdca697d6f324222b0eca
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-11 21:47:04 +0600

    Some Update

---
+++ frontEnd/JAVASCRIPT/Assignments/Assignment-03/p.js
@@ -0,0 +1,8 @@
+function bonusScore(scores) {
+  if (!Array.isArray(scores) || scores.length === 0) {
+    return "Invalid";
+  }
+  if (!scores.every((score) => typeof scores === "number")) {
+    return "Invalid";
+  }
+}
