560d6fc5e8ce8f4af0b8be52277fc07f195bcae8
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-14 20:20:14 +0600

    Some Update

--- frontEnd/JAVASCRIPT/Assignments/Assignment-03/p.js
+++ frontEnd/JAVASCRIPT/Assignments/Assignment-03/p.js
@@ -1,14 +1,14 @@
-// function bonusScore(scores) {
-//   if (!Array.isArray(scores) || scores.length === 0) {
-//     return "Invalid";
-//   }
-//   if (!scores.every((score) => typeof score === "number")) {
-//     return "Invalid";
-//   }
+function bonusScore(scores) {
+  if (!Array.isArray(scores) || scores.length === 0) {
+    return "Invalid";
+  }
+  if (!scores.every((score) => typeof score === "number")) {
+    return "Invalid";
+  }
 
-//   const updateScore = scores.map((score) => score + 10);
-//   const total = scores.reduce((sum, score) => sum + score, 0);
-//   return total;
-// }
+  const updateScore = scores.map((score) => score + 10);
+  const total = scores.reduce((sum, score) => sum + score, 0);
+  return total;
+}
 
-// console.log(bonusScore([80, 65, 90, 75]));
+console.log(bonusScore([80, 65, 90, 75]));
