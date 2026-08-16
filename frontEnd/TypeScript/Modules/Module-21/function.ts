fbd10631eef179499ed760dcd01b29b50255bd24
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-16 14:25:37 +0600

    Some Update

--- frontEnd/TypeScript/Modules/Module-21/function.ts
+++ frontEnd/TypeScript/Modules/Module-21/function.ts
@@ -2,7 +2,9 @@
 //     const total = num1 + num2
 //     return total
 // }
-function add(num1, num2) {
+function add(num1: number, num2: number) {
   const total = num1 + num2;
   return total;
 }
+
+const result: number = add(10, 20);
