5b0209c672131b8c47a94bdd5dc7aaa932550597
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-22 20:57:27 +0600

    Problem 2 solve

--- frontEnd/TypeScript/Assignments/ASSIGNMENT_04/problems.ts
+++ frontEnd/TypeScript/Assignments/ASSIGNMENT_04/problems.ts
@@ -9,3 +9,13 @@ function getBatteryStatus(percentage: number): string {
     return "Full";
   }
 }
+
+interface Booking {
+  name: string;
+  guests: number;
+  time: string;
+}
+
+function formatBookingConfirmation(booking: Booking): string {
+  return `${booking.name}'s table for ${booking.guests} guests is confirmed at ${booking.time}.`;
+}
